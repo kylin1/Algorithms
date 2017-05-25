6ce7b78f5df8adb88c1a46969c40e54cec07b0a6
kylin1 <18795853969@163.com>
2017-05-25 13:08:23 +0800

    init commit

---
+++ src/Ch1UnionFind/UnionFind.java
@@ -0,0 +1,9 @@
+package Ch1UnionFind;
+
+/**
+ * Created by kylin on 25/05/2017.
+ * All rights reserved.
+ */
+public class UnionFind {
+
+}
---
+++ test/Ch1UnionFind/UnionFindTest.java
@@ -0,0 +1,16 @@
+package Ch1UnionFind;
+
+import org.junit.Test;
+
+/**
+ * Created by kylin on 25/05/2017.
+ * All rights reserved.
+ */
+public class UnionFindTest {
+
+    @Test
+    public void test(){
+
+    }
+
+}
